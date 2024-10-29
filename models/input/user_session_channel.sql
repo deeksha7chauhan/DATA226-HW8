98582520cb184cdb22555f319b8621f92862869d
deeksha7chauhan <deeksha7chauhan@gmail.com>
2024-10-28 17:10:56 -0700

    Update user_session_channel.sql

--- models/input/user_session_channel.sql
+++ models/input/user_session_channel.sql
@@ -1,5 +1,12 @@
-SELECT
+WITH user_data AS (
+    SELECT
+        userId,
+        sessionId,
+        channel
+    FROM {{ source('raw_data', 'user_session_channel') }}
+)
+SELECT 
     userId,
     sessionId,
     channel
-FROM {{ source('raw_data', 'user_session_channel') }}
+FROM user_data;
